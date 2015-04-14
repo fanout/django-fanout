e39daa9c38a8fb92c1e4824721b7e515d0a1fd82
Kon <bokarius@comcast.net>
2015-04-14 13:45:01 -0700

    required latest fanout version

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
 name='django-fanout',
-version='1.1.0',
+version='1.1.1',
 description='Fanout.io library for Django',
 author='Justin Karneges',
 author_email='justin@fanout.io',
 url='https://github.com/fanout/django-fanout',
 license='MIT',
 py_modules=['django_fanout'],
-install_requires=['fanout>=1.1.0'],
+install_requires=['fanout>=1.1.1'],
 classifiers=[
 	'Topic :: Utilities',
 	'License :: OSI Approved :: MIT License'
